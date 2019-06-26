d11afcf191016f9d4b0dd1a98ea499f0626bb70d
wuyuanqiang <wuyuanqiang.lanyife.com>
2019-06-26 15:16:49 +0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+
+import { workspace, window, ExtensionContext, commands } from 'vscode';
+
+export function activate(context: ExtensionContext) {
+
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
